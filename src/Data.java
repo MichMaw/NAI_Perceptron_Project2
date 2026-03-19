6eb8c67ebf61e220a3f1fcb0fe7244e236586167
Michał Mach <s30662@pjwstk.edu.pl>
2026-03-19 22:18:55 +0800

    added loading data from file.data

---
+++ src/Data.java
@@ -0,0 +1,32 @@
+﻿public class Data {
+    private final double[] vectorValues;
+
+    private final String label;
+    Data(double[] vectorValues, String label){
+        this.vectorValues = vectorValues;
+        this.label = label;
+    }
+    Data(double[] vectorValues){
+        this.vectorValues = vectorValues;
+        this.label = "{Uninitialized}";
+    }
+    public double[] getVectorValues(){
+        return vectorValues;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(double e : vectorValues){
+            sb.append(e);
+            sb.append(",");
+        }
+        return sb.toString();
+    }
+
+    public String getLabel(){
+        return label;
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,52 @@
-﻿public class Main {
+﻿import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+
+    public static List<Data> loadDataSet(String filename) throws Exception {
+        List<Data> dataSet = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                dataSet.add(convertStringToData(line));
+            }
+        } catch (java.io.FileNotFoundException e) {
+            System.out.println("ERROR: couldn't load file " + filename + " make sure its in correct directory");
+        }
+        System.out.println("file Successfully added");
+        return dataSet;
+
+    }
+
+
+    public static Data convertStringToData(String line) {
+        String[] parts = line.split(",");
+
+
+        String label = parts[parts.length - 1];
+        double[] values = new double[parts.length - 1];
+
+        for (int i = 0; i < parts.length - 1; i++) {
+            values[i] = Double.parseDouble(parts[i]);
+        }
+        return new Data(values, label);
+    }
+
+
+    public static Data convertStringWithoutLableToData(String line){
+        String[] parts = line.split(",");
+        double[] values = new double[parts.length];
+        for (int i= 0; i < parts.length; i++){
+            values[i] = Double.parseDouble(parts[i]);
+        }
+        return new Data(values);
+    }
+
+
 }
