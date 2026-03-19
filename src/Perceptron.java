d58827566420be444790c28a54b293839b4912b5
Michał Mach <s30662@pjwstk.edu.pl>
2026-03-19 20:53:09 +0800

    added learn predict method skeletons

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,2 +1,23 @@
 ﻿public class Perceptron {
+    double threshold;
+    double[] weights;
+    double learningRate;
+    Perceptron(){
+
+    }
+    public int predict(double[] inVector){
+        double output =- threshold;
+        for(int i=0; i< weights.length;i++)
+           output += weights[i] *inVector[i];
+        return output >=0  ? 1: 0;
+    }
+    public void  learn(double[] vectorToLearnFrom,int target) {
+        int predictedInt = this.predict(vectorToLearnFrom);
+        int error = target - predictedInt;
+        if(error!=0){
+            for(int j = 0; j <weights.length; j++){
+                weights[j] += error * learningRate * vectorToLearnFrom[j];
+            }
+        }
+    }
 }
